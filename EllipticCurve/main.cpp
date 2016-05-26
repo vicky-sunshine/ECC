b206250c5469a5cd855c7ee98a8677f74685f379
vicky <lch82327@gmail.com>
2016-05-27 01:57:44 +0800

    a roundtrip for ecc

--- EllipticCurve/main.cpp
+++ EllipticCurve/main.cpp
@@ -1,32 +1,56 @@
 #include "BigNumber.hpp"
 #include "Point.hpp"
+#include "FiniteFieldElement.hpp"
 #include "EllipticCurve.hpp"
 
 int main(int argc, char** argv){
 
   //demo
   BigNumber p =  BigNumber("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7FFFFFFF");
-  BigNumber a =  BigNumber("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7FFFFFFC");
-  BigNumber b =  BigNumber("1C97BEFC54BD7A8B65ACF89F81D4D4ADC565FA45");
-  BigNumber Gx = BigNumber("4A96B5688EF573284664698968C38BB913CBFC82");
-  BigNumber Gy = BigNumber("23A628553168947D59DCC912042351377AC5FB32");
+  FiniteFieldElement::mod_prime = p;
+  
+  FiniteFieldElement a =  FiniteFieldElement("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7FFFFFFC");
+  FiniteFieldElement b =  FiniteFieldElement("1C97BEFC54BD7A8B65ACF89F81D4D4ADC565FA45");
+  FiniteFieldElement Gx = FiniteFieldElement("4A96B5688EF573284664698968C38BB913CBFC82");
+  FiniteFieldElement Gy = FiniteFieldElement("23A628553168947D59DCC912042351377AC5FB32");
   BigNumber n =  BigNumber("0100000000000000000001F4C8F927AED3CA752257");
   Point g = Point(Gx,Gy);
   EllipticCurve ec = EllipticCurve(p,a,b,g,n);
 
-  BigNumber na = BigNumber("3C870C3E99245E0D1C06B747DEB3124DC843BB8B"); // private key
-  BigNumber nk = BigNumber("A61F035A7D0938251F5DD4CBFC96F5453B130D89"); 
-  BigNumber mx = BigNumber("2923BE84E16CD6AE529049F1F1BBE9EBB3A6DB01");
-  BigNumber my = BigNumber("A38CC9D23D61B446A4F51B2C8DA6BC6FC5CA2BAC");
-  // Point m = Point(mx,my);
-  // ec.elgamel_encrypt(m, ec.mult_n(g, na));
-  BigNumber sum1 = (mx*mx*mx + a*mx+b)%p;
-  BigNumber sum2 = (my*my) % p;
-  std::cout << sum1 << std::endl;
-  std::cout << sum2 << std::endl;
-  BigNumber m = BigNumber("2923BE84E16CD6AE529049F1F1BBE9EBB3A6DB00");
   
+  std::cout << "<EC-ElGamal encryption>" << std::endl;
   
+  // encrypt input
+  std::string m = "FECDFF7C8E6F2C1DC6F7D5C3987AEDFC324DF6";
+  std::string Pa_encode = "039994C5C16070EE878F89A6143CE865AC2EC7EC5D"; // public key
+  BigNumber nk = BigNumber("5487CF3D6F9E4F1C3DAEF5C3CF7D6FC33C675DC6");
+  std::cout << "Plaintext M: " << m << std::endl;
+  std::cout << "Pa: " << Pa_encode << std::endl;
+  std::cout << "nk: " << nk << std::endl;
+  
+  // encrypt
+  Point Pm = ec.data_embedded(m);
+  std::cout << "Mx: " << Pm.x << std::endl;
+  std::cout << "My: " << Pm.y << std::endl;
 
+  
+  Point Pa = ec.decode(Pa_encode); // public key
+  std::vector<Point> Cm =  ec.elgamel_encrypt(Pm, nk, Pa);
+  std::cout << "Pk: " << ec.encode(Cm[0]) << std::endl;
+  std::cout << "Pb: " << ec.encode(Cm[1]) << std::endl;
+  
+  
+  std::cout << "\n<EC-ElGamal decryption>" << std::endl;
+
+  // decrypt input
+  BigNumber na = BigNumber("3C870C3E99245E0D1C06B747DEB3124DC843BB8B"); // private key
+  std::cout << "na: " << na << std::endl;
+  
+  // decrypt
+  Point Pm_after = ec.elgamel_decrypt(Cm, na);
+  FiniteFieldElement m_after = ec.data_unembedded(Pm_after);
+  std::cout << "Plaintext:\n" << m_after << std::endl;
+  
+  
   return 0;
 }
