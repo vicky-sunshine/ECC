35c4552aef01a4cb034a7c6c6cafc4aa523df1ba
vicky <lch82327@gmail.com>
2016-05-27 01:26:30 +0800

    add power, sqrt, inverse method ofor Finite Field Element

--- FiniteFieldElement/FiniteFieldElement.cpp
+++ FiniteFieldElement/FiniteFieldElement.cpp
@@ -18,15 +18,15 @@ std::vector<BigNumber> FiniteFieldElement::extend_eculidean(BigNumber a, BigNumb
     temp1= a%b;
     a=b;
     b=temp1;
-    
+
     temp2 = x;
     x=lastx-q*x;
     lastx = temp2;
-    
+
     temp3 = y;
     y = lasty-q*y;
     lasty=temp3;
-    
+
   }
   std::vector<BigNumber> result{lastx, lasty};
   return result;
@@ -35,6 +35,10 @@ std::vector<BigNumber> FiniteFieldElement::extend_eculidean(BigNumber a, BigNumb
 
 
 // constructors
+FiniteFieldElement::FiniteFieldElement() {
+  this->i = BigNumber(0);
+}
+
 FiniteFieldElement::FiniteFieldElement(long long input) {
   this->i = input % mod_prime;
 }
@@ -43,6 +47,7 @@ FiniteFieldElement::FiniteFieldElement(const std::string& input) {
   this->i = BigNumber(input) % mod_prime;
 
 }
+
 FiniteFieldElement::FiniteFieldElement(BigNumber input) {
   this->i = input % mod_prime;
 }
@@ -52,18 +57,23 @@ FiniteFieldElement::FiniteFieldElement(BigNumber input) {
 bool operator==(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   return lhs.i == rhs.i;
 }
+
 bool operator!=(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   return lhs.i != rhs.i;
 }
+
 bool operator>(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   return lhs.i > rhs.i;
 }
+
 bool operator<(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   return lhs.i < rhs.i;
 }
+
 bool operator>=(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   return lhs.i >= rhs.i;
 }
+
 bool operator<=(const FiniteFieldElement& lhs, const FiniteFieldElement&rhs) {
   return lhs.i <= rhs.i;
 }
@@ -73,25 +83,28 @@ bool operator<=(const FiniteFieldElement& lhs, const FiniteFieldElement&rhs) {
 const FiniteFieldElement operator+(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   BigNumber add = lhs.i + rhs.i;
   if (add < 0) {
-    add = (rhs.mod_prime + add) % rhs.mod_prime;
+    add = rhs.mod_prime + add;
   }
 
   return FiniteFieldElement(add % rhs.mod_prime);
 }
+
 const FiniteFieldElement operator-(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   BigNumber sub = lhs.i - rhs.i;
   if (sub < 0) {
-    sub = (rhs.mod_prime + sub) % rhs.mod_prime;
+    sub = rhs.mod_prime + sub ;
   }
-  return FiniteFieldElement(sub);
+  return FiniteFieldElement(sub% rhs.mod_prime);
 }
+
 const FiniteFieldElement operator*(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   BigNumber mul = lhs.i * rhs.i;
   if (mul < 0) {
-    mul = (rhs.mod_prime + mul) % rhs.mod_prime;
+    mul = rhs.mod_prime + mul;
   }
-  return FiniteFieldElement(mul);
+  return FiniteFieldElement(mul % rhs.mod_prime);
 }
+
 const FiniteFieldElement operator/(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
   BigNumber rhs_inv = inv(rhs).i;
   BigNumber div = lhs.i * rhs_inv % rhs.mod_prime;
@@ -99,12 +112,13 @@ const FiniteFieldElement operator/(const FiniteFieldElement& lhs, const FiniteFi
 }
 
 const FiniteFieldElement operator%(const FiniteFieldElement& lhs, const FiniteFieldElement& rhs) {
-  BigNumber mod = lhs.i - rhs.i;
+  BigNumber mod = lhs.i % rhs.i;
   if (mod < 0) {
-    mod = (rhs.mod_prime + mod) % rhs.mod_prime;
+    mod = rhs.mod_prime + mod;
   }
-  return FiniteFieldElement(mod);
+  return FiniteFieldElement(mod % rhs.mod_prime);
 }
+
 const FiniteFieldElement operator-(const FiniteFieldElement& rhs) {
   BigNumber sub = (rhs.mod_prime - rhs.i) % rhs.mod_prime;
   return FiniteFieldElement(sub);
@@ -116,11 +130,46 @@ const FiniteFieldElement inv(const FiniteFieldElement& rhs) {
   if (inv < 0 ) {
     inv =  (rhs.mod_prime + inv) % rhs.mod_prime;
   }
-    
+
   return FiniteFieldElement(inv);
 }
 
+std::vector<FiniteFieldElement> sqrt(FiniteFieldElement& input) {
+
+  BigNumber power = (input.mod_prime+1)/4;
+  FiniteFieldElement r1 = pow(input, power);
+  FiniteFieldElement r2 = (FiniteFieldElement::mod_prime - r1);
+  std::vector<FiniteFieldElement> root{r1,r2};
+
+  return root;
+}
+
+FiniteFieldElement pow(FiniteFieldElement& base, BigNumber& power) {
+  FiniteFieldElement sum = base;
+  if  (power ==0) return 1;
+
+  FiniteFieldElement odd(1);
+  while (base!=1 && power>1){
+    if (power % 2 != 0){ // odd
+      odd = odd * base;
+    }
+    power = power / 2;
+    base = base * base;
+  }
+
+  return base * odd;
+}
+
+// output
 std::ostream& operator<<(std::ostream& os, const FiniteFieldElement& rhs) {
   os << rhs.i;
   return os;
+}
+
+std::string FiniteFieldElement::to_string() const{
+  std::stringstream ss;
+  std::string str;
+  ss << (*this).i;
+  ss >> str;
+  return str;
 }
--- FiniteFieldElement/FiniteFieldElement.hpp
+++ FiniteFieldElement/FiniteFieldElement.hpp
@@ -2,6 +2,7 @@
 #define FiniteFieldElement_hpp
 
 #include <iostream>
+#include <sstream>
 #include "BigNumber.hpp"
 
 class FiniteFieldElement{
@@ -13,10 +14,11 @@ public:
   BigNumber i;
 
   // constructors
+  FiniteFieldElement();
   FiniteFieldElement(long long); // directly convert from an int
   FiniteFieldElement(const std::string&);
   FiniteFieldElement(BigNumber);
-  
+
   // overloaded logical operators as member functions
   friend bool operator==(const FiniteFieldElement&, const FiniteFieldElement&);
   friend bool operator!=(const FiniteFieldElement&, const FiniteFieldElement&);
@@ -24,7 +26,7 @@ public:
   friend bool operator<(const FiniteFieldElement&, const FiniteFieldElement&);
   friend bool operator>=(const FiniteFieldElement&, const FiniteFieldElement&);
   friend bool operator<=(const FiniteFieldElement&, const FiniteFieldElement&);
-  
+
   // overloaded arithmetic operators as member functions
   friend const FiniteFieldElement operator+(const FiniteFieldElement&, const FiniteFieldElement&);
   friend const FiniteFieldElement operator-(const FiniteFieldElement&, const FiniteFieldElement&);
@@ -34,12 +36,13 @@ public:
   friend const FiniteFieldElement operator-(const FiniteFieldElement&);
 
   friend const FiniteFieldElement inv(const FiniteFieldElement&);
-  
-  // ouput format for BigNumber
+  friend std::vector<FiniteFieldElement> sqrt(FiniteFieldElement&);
+  friend FiniteFieldElement pow(FiniteFieldElement& base, BigNumber& power);
+
+  // ouput format for FiniteFieldElement
   friend std::ostream& operator<<(std::ostream&, const FiniteFieldElement&);
-  
-  
+  std::string to_string() const;
+
 };
 
 #endif /* FiniteFieldElement_hpp */
-
