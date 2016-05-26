ede09bd721124c77fd6fdfbc622a2ed12d433a91
vicky <lch82327@gmail.com>
2016-05-27 01:29:20 +0800

    arithmetic element change from BigNumber to FiniteField and add encode/decode method

--- EllipticCurve/EllipticCurve.cpp
+++ EllipticCurve/EllipticCurve.cpp
@@ -1,8 +1,18 @@
 #include "EllipticCurve.hpp"
 
+// private format conversion handler
+const std::vector<Point> EllipticCurve::depressToPoint(const FiniteFieldElement& input) {
+  
+  FiniteFieldElement sum = input * input * input + a * input + b;
+  std::vector<FiniteFieldElement> root = sqrt(sum);
+  
+  std::vector<Point> result = {Point(input,root[0]),Point(input,root[1])};
+  return result;
+}
 
-// EllipticCurve class
-EllipticCurve::EllipticCurve(BigNumber& mod_prime, BigNumber& a, BigNumber& b, Point& generator, BigNumber& order) {
+// EllipticCurve Constructor
+EllipticCurve::EllipticCurve(BigNumber& mod_prime, FiniteFieldElement& a, FiniteFieldElement& b, Point& generator, BigNumber& order) {
+  FiniteFieldElement::mod_prime = mod_prime;
   this->mod_prime = mod_prime;
   this->a = a;
   this->b = b;
@@ -10,49 +20,69 @@ EllipticCurve::EllipticCurve(BigNumber& mod_prime, BigNumber& a, BigNumber& b, P
   this->order = order;
 }
 
+// Point Operation
 const bool EllipticCurve::isOnCurve(const Point& p) {
-  BigNumber sum;
-  
+  FiniteFieldElement sum;
+
   // y^2 mod prime =? x^3 + ax + b mod prime
-  return (p.y * p.y) % mod_prime == ( p.x * p.x * p.x + a * p.x + b ) % mod_prime;
+  return (p.y * p.y) == ( p.x * p.x * p.x + a * p.x + b );
+}
+
+const bool EllipticCurve::isZero(const Point& p) {
+  return p.x==0 && p.y==0;
 }
 
 const Point EllipticCurve::negative(const Point& p) {
-  return Point(p.x, (-1 * p.y) % mod_prime);
+  return Point(p.x, -p.y);
 }
 
 const Point EllipticCurve::addition(const Point& p, const Point& q) {
   Point r;
-  BigNumber s;
-  
-  if ( p == q) return doubling(p);
-  
-  s = ((p.y - q.y) / (p.x -q.x)) % mod_prime;
-  r.x = (s * s - p.x - q.x) % mod_prime;
-  r.y = (s * (p.x - r.x) - p.y) % mod_prime;
-  
+  FiniteFieldElement s;
+
+  if (p == q) return doubling(p);
+
+  if (isZero(p)) {
+    return q;
+  }
+  if (isZero(q)) {
+    return p;
+  }
+
+  if (p.x==q.x && p.y == -1*q.y) {
+    return Point(0,0);
+  }
+
+  s = (p.y - q.y) /  (p.x - q.x);
+  r.x = (s * s - p.x - q.x);
+  r.y = (s * (p.x - r.x) - p.y);
+
   return r;
 }
 
 const Point EllipticCurve::doubling(const Point& p) {
   Point r;
-  BigNumber s;
-  s = (3 * p.x * p.x + a) / (2 * p.y ) % mod_prime;
-  r.x = (s * s - 2 * p.x) % mod_prime;
-  r.y = (s * (p.x - r.x) - 1 * p.y) % mod_prime;
+  FiniteFieldElement s;
+
+  if (isZero(p)) {
+    return p;
+  }
+  
+  s = (3 * p.x * p.x + a ) /  (2 * p.y);
+  r.x = s * s - 2 * p.x;
+  r.y = s * (p.x - r.x) - p.y;
   return r;
 }
 
 const Point EllipticCurve::mult_n(const Point& p, const BigNumber& n) {
   if (n == 1) return p;
   if (n == 2) return doubling(p);
-  
+
   // init odd as ZERO
   Point sum = p;
-  Point odd = p;
-  BigNumber num = n - 1;
-
-  while (num > 2){
+  Point odd = Point (0,0);
+  BigNumber num = n;
+  while (num > 1){
     if (num % 2 != 0){ // odd
       odd = EllipticCurve::addition(sum ,odd);
     }
@@ -60,22 +90,105 @@ const Point EllipticCurve::mult_n(const Point& p, const BigNumber& n) {
     sum = EllipticCurve::doubling(sum);
   }
   sum = EllipticCurve::addition(sum, odd);
+
   return sum;
 }
 
-const std::vector<Point> EllipticCurve::elgamel_encrypt(Point m, Point public_key) {
+
+// Data embedded and unembedded
+const Point EllipticCurve::data_embedded(std::string& data) {
+  
+  FiniteFieldElement Mx = FiniteFieldElement(data) * 256;
+
+  std::vector<Point> M;
+  while(1){
+    M = depressToPoint(Mx);
+
+
+    if(isOnCurve(M[0]) && (M[0].y % 2 == 1)) {
+      return M[0];
+    }
+    if(isOnCurve(M[1]) && (M[1].y % 2 == 1)) {
+      return M[1];
+    }
+    Mx = Mx+1;
+  }
+
+}
+
+const std::string EllipticCurve::data_unembedded(Point& plaintext) {
+  
+  std::string result = plaintext.x.to_string();
+  result.erase(result.end()-2, result.end());
+  return result;
+}
+
+
+// format conversion
+const Point EllipticCurve::decode(const std::string& input) {
+
+  char ch = input[1]; // choose prefix
+  
+  // remove prfix to get x of Point
+  std::string tempstr = input;
+  tempstr.erase(tempstr.begin(),tempstr.begin()+2);
+  FiniteFieldElement num = FiniteFieldElement(tempstr);
+  
+  // get two possible points
+  std::vector<Point> result = depressToPoint(num);
+
+  // choose y depends on prefix, 03->odd, 02->even
+  if (result[0].y % 2 == 1) {
+    // root[0] is odd
+    if (ch=='3') {
+      return Point(num,result[0].y);
+    } else {
+      return Point(num,result[1].y);
+    }
+  } else {
+    // root[0] is even
+    if (ch=='3') {
+      return Point(num,result[1].y);
+    } else {
+      return Point(num,result[0].y);
+    }
+  }
+}
+
+const std::string EllipticCurve::encode(const Point& input) {
+  std::string str = input.x.to_string();
+
+  if (input.y % 2 == 0) {
+    str.insert(0, "02");
+  } else {
+    str.insert(0, "03");
+  }
+  
+  return str;
+}
+
+
+const std::vector<Point> EllipticCurve::elgamel_encrypt(Point& plaintext, BigNumber& nk, Point& public_key) {
   std::vector<Point> cipher;
-  
-  // random nk
-  BigNumber nk = BigNumber("A61F035A7D0938251F5DD4CBFC96F5453B130D89");
-  Point c1 = EllipticCurve::mult_n(generator, nk);
-  Point c2 = EllipticCurve::addition(m, EllipticCurve::mult_n(public_key, nk));
-  
+
+  Point Pk = EllipticCurve::mult_n(generator, nk);
+  Point P_pa_nk = EllipticCurve::mult_n(public_key, nk);
+  Point Pb = EllipticCurve::addition(plaintext, P_pa_nk);
+
   // std::cout << c1 << std::endl;
   // std::cout << c2 << std::endl;
-  cipher.push_back(c1);
-  cipher.push_back(c2);
-  
+  cipher.push_back(Pk);
+  cipher.push_back(Pb);
+
   return cipher;
 }
 
+const Point EllipticCurve::elgamel_decrypt(std::vector<Point>& ciphertext, BigNumber& private_key) {
+  Point pk = ciphertext[0];
+  Point pb = ciphertext[1];
+  
+  // Pm= Pb - na * Pk
+  Point neg_pk = negative(mult_n(pk,private_key));
+  Point pm = addition(pb, neg_pk);
+  return pm;
+}
--- EllipticCurve/EllipticCurve.hpp
+++ EllipticCurve/EllipticCurve.hpp
@@ -10,6 +10,7 @@
 #define EllipticCurve_hpp
 
 #include <iostream>
+#include "FiniteFieldElement.hpp"
 #include "BigNumber.hpp"
 #include "Point.hpp"
 
@@ -18,36 +19,41 @@
 class EllipticCurve{
 private:
   BigNumber mod_prime;
-  BigNumber a;
-  BigNumber b;
+  FiniteFieldElement a;
+  FiniteFieldElement b;
   Point generator;
   BigNumber order;
-  
+
+  // Format Conversion
+  const std::vector<Point> depressToPoint(const FiniteFieldElement&);
+  const FiniteFieldElement compressToFiniteFieldElement(const Point&);
+
 public:
   // constructors
   // p,a,b,G,n
-  EllipticCurve(BigNumber& mod_prime, BigNumber& a, BigNumber& b, Point& G, BigNumber& order);
-  
-  // Point Operation
+  EllipticCurve(BigNumber& mod_prime, FiniteFieldElement& a, FiniteFieldElement& b, Point& G, BigNumber& order);
+
+  // Point Operation in this Elliptic Curve
   const bool isOnCurve(const Point&);
+  const bool isZero(const Point&);
   const Point addition(const Point&, const Point&);
   const Point negative(const Point&);
   const Point doubling(const Point&);
   const Point mult_n(const Point&, const BigNumber&);
-  
 
-  //
-  std::vector<BigNumber> find_root(BigNumber&);
-  
-  
+  // Data embedded and unembedded
+  const Point data_embedded(std::string& data);
+  const std::string data_unembedded(Point& plaintext);
+
   // Format Conversion
-  const Point depressToPoint(const BigNumber&);
-  const BigNumber compressToBigNumber(const Point&);
+  const Point decode(const std::string&);
+  const std::string encode(const Point&);
 
   // encrypt and decrypt
-  const std::vector<Point> elgamel_encrypt(Point m, Point public_key);
-  
-  // ouput format for BigNumber
+  const std::vector<Point> elgamel_encrypt(Point& plaintext, BigNumber& nk, Point& pa);
+  const Point elgamel_decrypt(std::vector<Point>& c, BigNumber& public_key);
+
+  // ouput format for FiniteFieldElement
   friend std::ostream& operator<<(std::ostream&, const EllipticCurve&);
 };
 
